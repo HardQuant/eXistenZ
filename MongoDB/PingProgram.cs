95794a1347bf492fde412a7fe21b841609927016
samuelsalmeron <catplierdotcom@outlook.com>
2024-10-21 11:54:53 -0500

    altered the Exception message

--- MongoDB/PingProgram.cs
+++ MongoDB/PingProgram.cs
@@ -11,7 +11,7 @@ class Program
 
         if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password) || string.IsNullOrEmpty(cluster))
         {
-            Console.WriteLine("Missing Required environment variables for MongooseDb credentials.Please enter the following line into the Terminal: ~/.zshrc");
+            Console.WriteLine("Missing Required environment variables for MongooseDb credentials.Please enter the following line into the Terminal: source ~/.zshrc");
             return;
         }
         string connectionUri = $"mongodb+srv://{username}:{Uri.EscapeDataString(password)}@{cluster}/?retryWrites=true&w=majority&appName=Cluster44";
