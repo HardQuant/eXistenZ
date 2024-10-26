2ae38adf2123fdeef9632d0f1ad8a0b948e12566
samuelsalmeron <catplierdotcom@outlook.com>
2024-10-26 18:20:18 -0500

    Creating new class instance and calling relevant methods

--- MongoDB/PingProgram.cs
+++ MongoDB/PingProgram.cs
@@ -25,6 +25,11 @@ class Program
         {
             var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
             Console.WriteLine($"Pinged your deployment. You succesfully connected to MongoDB at [{DateTime.Now}]!");
+
+            //Initializing Database and collection
+            var DatabaseSetup = new DatabaseSetup(client, "F_1");
+            DatabaseSetup.CreateCollection("Drivers");
+            DatabaseSetup.SeedInitialData();
         }
         catch (Exception ex)
         {
